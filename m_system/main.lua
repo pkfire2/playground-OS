4664764c627645a6d8724c7cde53c708cb1d1d89
vombagel64 <Moodysnowman@outlook.com>
2026-05-12 21:42:23 -0400

    So it begins... I've got the basic structure of the project set up, and I'm starting to work on the main.lua file. I've also made a custom cursor, added a .gitignore file, and created a todo file to keep track of what I need to do! I'm really excited to see how this project will go! It's very ambitious and big, and probably WAY above my skill level right now... But I'm going to try my best and learn as much as I can along the way!

---
+++ m_system/main.lua
@@ -0,0 +1,36 @@
+
+function love.load()
+    -- controls the state of vomOS. Ex: boot, app, etc.
+    local vState = "startup"
+
+    canEsc = true
+   
+    love.window.setFullscreen(false)
+
+    love.graphics.newImage("m_assets/cursor.png")
+    
+    local cr_data = love.image.newImageData("m_assets/cursor.png")
+    cursor = love.mouse.newCursor(cr_data, 6, 6)
+    love.mouse.setCursor(cursor)
+
+    love.window.setIcon(love.image.newImageData("m_assets/cursor.png"))
+    love.window.setTitle("playground test window")
+end
+
+function love.update(dt)
+    
+end
+
+function love.draw()
+    love.graphics.setBackgroundColor(0.945, 0.910, 0.835)
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.print("press ESC to end the program", 0, 20)
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        if canEsc == true then
+            love.event.quit()
+        end
+    end
+end
