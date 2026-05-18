b43efa8fe5fae5c7f6e99060de07deab2d365a10
vombagel64 <Moodysnowman@outlook.com>
2026-05-17 20:46:41 -0400

    this version is kinda broken, it now plays the startup sound upon opening the program... but it NEVER stops playig and i dont know how to stop it. if anyone knows how to stop a sound from playing in love2d please tell me other than that i changed a few lines

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# What is Playground?
-Playground is planned to be a fake OS that lets you make your own apps, games, whatever you can think of, and actually run them inside the "OS"!
+# What is playground-os?
+playground-os is planned to be a terminal-focused fake OS that lets you make your own apps, games, whatever you can think of, and run them inside the program!
+apps are self-contained in their own folders with their own files and assets!
+
+## How do I make my own apps?
+proper documentation will come with the first official release, but for now I'm adding as may comments as i can to explain everything while im building this.
+for now... sit tight lmao
 
 ## DISCLAIMER: 
 Progress is going to be VERY, VERY slow for now. I'm only a beginner, so I'm still figuring everything out.
-Please be patient and stay tuned!!
+I honestly dont know how I came up with this crazy idea and how I'm going to do it...
+so please be patient and stay tuned if you're interested!!
 
-proper documentation will come with the first official release, but for now I'm addin as may comments as i can to explain everything
--- m_system/main.lua
+++ m_system/main.lua
@@ -6,8 +6,8 @@ function love.load()
     canEsc = true
     
     -- set up the window
-    love.window.setMode(800, 800, {resizable=true})
-    love.window.setFullscreen(false)
+    love.window.setMode(800, 600, {resizable=true})
+    love.window.setFullscreen(true)
     love.window.setIcon(love.image.newImageData("m_assets/playIcon.png"))
     love.window.setTitle("playgroundtest")
     
@@ -16,27 +16,41 @@ function love.load()
     cursor = love.mouse.newCursor(cr_data, 4, 1)
     love.mouse.setCursor(cursor)
 
-    local defaultFont = love.graphics.newFont("fonts/W95F.otf", 16)
+    local defaultFont = love.graphics.newFont("fonts/W95F.otf", 23)
     love.graphics.setFont(defaultFont)
 
     -- load in audio files
-    love.audio.newSource("m_sfx/startsound.mp3", "static")
-    love.audio.newSource("m_sfx/openapp.mp3", "static")
-    love.audio.newSource("m_sfx/closeapp.mp3", "static")
-    love.audio.newSource("m_sfx/notif.mp3", "static")
-    love.audio.newSource("m_sfx/crashsound.mp3", "static")
+    startSound = love.audio.newSource("m_sfx/startsound.mp3", "static")
+    openAppSound = love.audio.newSource("m_sfx/openapp.mp3", "static")
+    closeAppSound = love.audio.newSource("m_sfx/closeapp.mp3", "static")
+    notifSound = love.audio.newSource("m_sfx/notif.mp3", "static")
+    crashSound = love.audio.newSource("m_sfx/crashsound.mp3", "static")
 
+    function loadTerminal() -- load in terminal file for later
+        require("m_system.terminal")
+    end
+    
+    function startup()
+        -- play the startup sound
+        love.audio.play(startSound)
+        -- wait for the sound to finish before changing the state
+        love.timer.sleep(3)
+        state = "desktop"
+    end
 
 end
 
 function love.update(dt)
-    
+    if state == "startup" then
+        startup()
+    end
 end
 
 function love.draw()
-    love.graphics.setBackgroundColor(0.945, 0.910, 0.835)
+    love.graphics.setBackgroundColor(1, 1, 0, 1)
     love.graphics.setColor(0, 0, 0)
-    love.graphics.print("press ESC to end the program", 0, 0)
+    love.graphics.print("playground-os test window", 0, 0)
+    love.graphics.print("press ESC to end the program", 0, 25)
 end
 
 function love.keypressed(key)
