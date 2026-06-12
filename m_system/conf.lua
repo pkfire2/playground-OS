3664753430b280411ee509704e6c73acb2df2398
frendo <Moodysnowman@outlook.com>
2026-06-12 13:03:23 -0400

    Change window icon to oslogo.png

--- m_system/conf.lua
+++ m_system/conf.lua
@@ -4,5 +4,5 @@ function love.conf(t)
     t.window.resizable = false
     t.window.fullscreen = false
     t.window.title = "playground-os pre alpha v1"
-    t.window.icon = "m_assets/pg_icon.png"
+    t.window.icon = "m_assets/oslogo.png"
 end
